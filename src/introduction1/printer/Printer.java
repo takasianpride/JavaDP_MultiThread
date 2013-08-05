83e8c29d0b2de7a1087a92af75e68778bb2a46d0
takasianpride <takasy328@gmail.com>
2013-08-05 17:13:41 +0900

    P.14 スレッドを起動する(2) Runnableインターフェースを使う

---
+++ src/introduction1/printer/Printer.java
@@ -0,0 +1,11 @@
+package introduction1.printer;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: takasianpride
+ * Date: 2013/08/05
+ * Time: 17:09
+ * To change this template use File | Settings | File Templates.
+ */
+public class Printer {
+}
